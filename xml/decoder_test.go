8486647e486ee1f9d3e8ada7370d982fb99b2ff3
Randy Reddig <randy.reddig@fastly.com>
2023-12-07 11:13:36 -0800

    xml: add complex example

--- xml/decoder_test.go
+++ xml/decoder_test.go
@@ -1,6 +1,7 @@
 package xml
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 
@@ -41,6 +42,45 @@ func (s *Simple) DecodeField(dec codec.Decoder, i int, name string) error {
 		return dec.Decode(&s.Name)
 	case "here":
 		return dec.Decode(&s.Here)
+	}
+	return nil
+}
+
+func TestDecoderComplex(t *testing.T) {
+	x := `<complex length="99"><simple age="1" name="hello" here="true" /></complex>`
+	want := Complex{99, Simple{Age: 1, Name: "hello", Here: true}}
+	var v Complex
+	dec := NewDecoder(strings.NewReader(x))
+	err := dec.Decode(&v)
+	if err != nil {
+		t.Error(err)
+	}
+	if v != want {
+		t.Errorf("Decode: got %v, expdected %v", v, want)
+	}
+}
+
+type Complex struct {
+	Length int
+	Simple Simple
+}
+
+func (c *Complex) DecodeElement(dec codec.Decoder, i int, name string) error {
+	fmt.Printf("DecodeElement(dec, %d, %q)\n", i, name)
+	switch name {
+	case "complex":
+		return dec.Decode(c)
+	case "simple":
+		return dec.Decode(&c.Simple)
+	}
+	return nil
+}
+
+func (c *Complex) DecodeField(dec codec.Decoder, i int, name string) error {
+	fmt.Printf("DecodeField(dec, %d, %q)\n", i, name)
+	switch name {
+	case "length":
+		return dec.Decode(&c.Length)
 	}
 	return nil
 }
